027aa90a04a70a7f4a6f730cd27bd67f35926b5c
Wiktor Pacewicz <wiktor.pacewicz@microsoft.wsei.edu.pl>
2024-08-12 21:58:04 +0200

    Update infrastructure

---
+++ OrderFlow Client Hearts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrderFlow_Client_Hearts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ OrderFlow Server MainHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrderFlow_Server_MainHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
